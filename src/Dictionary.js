84acfd504af1abb7ea2724ee3b1f7e780f35e02e
jadeirons <105827385+jadeirons@users.noreply.github.com>
2022-09-25 16:23:56 -0400

    Added search function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div>Hello</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${searchTerm}`);
+  }
+
+  function handleSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="Search"
+          className="search"
+          placeholder="Search for a word..."
+          onChange={handleSearchTerm}
+        ></input>
+      </form>
+    </div>
+  );
 }
